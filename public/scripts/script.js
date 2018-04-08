e7427bb985c973cf7d57d9cfcda30b9af3f6bc61
Hsin Wang <hsinwang5@gmail.com>
2018-04-08 16:01:01 -0400

    added full responsiveness to navbar

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,14 @@
-const navLinks = document.querySelectorAll(".nav-default a");
+const navLinks = document.querySelectorAll(".nav-item");
 
 for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener("mouseover", function(e) {
-        this.parentNode.childNodes[3].classList.add("highlight-hover");
+        console.log(this.childNodes[1]);
+        this.childNodes[1].classList.add("highlight-hover");
     });
 }
 
 for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener("mouseout", function(e) {
-        this.parentNode.childNodes[3].classList.remove("highlight-hover");
+        this.childNodes[1].classList.remove("highlight-hover");
     });
 }
