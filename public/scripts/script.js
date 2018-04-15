ee42a13fff938896f036402df882427412c41a68
Hsin Wang <hsinwang5@gmail.com>
2018-04-15 02:34:53 -0400

    Add info panels and adverts to index page

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,21 +1,49 @@
-//Navbar behavior===========================================
-const navLinks = document.querySelectorAll(".nav-item");
+let mousedown;
+let mouseup;
+if ('ontouchstart' in window) {
+    mousedown = "touchstart"
+    mouseup = "touchend"
+} else {
+    mousedown = "mousedown"
+    mosueup = "mouseup"
+}
+
+const text = document.querySelector("html");
+const text1 = document.querySelector(".splash-image__text1");
+const trigger = text1.getBoundingClientRect().y;
+window.addEventListener("scroll", function(){
+    if (text.scrollTop > trigger) {
+        console.log("triggerd!!");
+    }
+});
+
+//Navbar behavior===========================================================
+const navLinks = document.querySelectorAll(".js-nav-item");
 
 for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener("mouseover", function(e) {
         this.childNodes[1].classList.add("highlight-hover");
     });
 }
-
 for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener("mouseout", function(e) {
         this.childNodes[1].classList.remove("highlight-hover");
     });
 }
 
-//showcase panels behavior=================================
-const showcaseBox1 = document.querySelectorAll(".showcase-box1");
-const showcaseBox2 = document.querySelectorAll(".showcase-box2");
+//iced milk frosties advert behavior=========================================
+const tryOneButton = document.querySelector(".js-text3");
+
+tryOneButton.addEventListener(mousedown, function(){
+    this.classList.add("pressed")
+});
+tryOneButton.addEventListener(mouseup, function(){
+    this.classList.remove("pressed")
+});
+
+//showcase panels behavior===================================================
+const showcaseBox1 = document.querySelectorAll(".js-showcase-box1");
+const showcaseBox2 = document.querySelectorAll(".js-showcase-box2");
 
 //upper diagonal
 for (let i = 0; i < showcaseBox1.length; i++) {
