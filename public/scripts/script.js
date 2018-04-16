66caafe3efa8bb5bdb33aaee386f3db0b189c93a
Hsin Wang <hsinwang5@gmail.com>
2018-04-16 00:04:07 -0400

    refactor all cdd to BEM and add responsiveness to everything

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -34,10 +34,10 @@ const linkButtons = document.querySelectorAll(".js-button-press");
 
 for (let i = 0; i < linkButtons.length; i++) {
     linkButtons[i].addEventListener(mousedown, function(){
-        this.classList.add("js-pressed")
+        this.classList.add("js-button-pressed")
     });
     linkButtons[i].addEventListener(mouseup, function(){
-        this.classList.remove("js-pressed")
+        this.classList.remove("js-button-pressed")
     });
 }
 
@@ -47,20 +47,20 @@ const showcaseBox2 = document.querySelectorAll(".js-showcase-box2");
 
 for (let i = 0; i < showcaseBox1.length; i++) {
     showcaseBox1[i].addEventListener("mouseenter", function(){
-        this.parentNode.childNodes[3].classList.add("showcase-box2-transform-down");
-        this.childNodes[1].classList.add("showcase-box1-image-transform");
+        this.parentNode.childNodes[3].classList.add("js-diagonal-slide-down");
+        this.childNodes[1].classList.add("js-top-image-transform");
     });
     showcaseBox1[i].addEventListener("mouseleave", function(){
-        this.parentNode.childNodes[3].classList.remove("showcase-box2-transform-down");
-        this.childNodes[1].classList.remove("showcase-box1-image-transform");
+        this.parentNode.childNodes[3].classList.remove("js-diagonal-slide-down");
+        this.childNodes[1].classList.remove("js-top-image-transform");
     });
     showcaseBox2[i].addEventListener("mouseenter", function(){
-        this.classList.add("showcase-box2-transform-up");
-        this.childNodes[1].classList.add("showcase-box2-image-transform");
+        this.classList.add("js-diagonal-slide-up");
+        this.childNodes[1].classList.add("js-bottom-image-transform");
     });
     showcaseBox2[i].addEventListener("mouseleave", function(){
-        this.classList.remove("showcase-box2-transform-up");
-        this.childNodes[1].classList.remove("showcase-box2-image-transform");
+        this.classList.remove("js-diagonal-slide-up");
+        this.childNodes[1].classList.remove("js-bottom-image-transform");
     });
 }
 
